0de62a11ef4c835a0ead3bea3190e9160027b6a3
Timeisfast <120894109+Timeisfast@users.noreply.github.com>
2024-02-21 02:01:23 +0900

    [Gold IV] Title: DSLR, Time: 2216 ms, Memory: 2336 KB -BaekjoonHub

---
+++ 백준/Gold/9019. DSLR/DSLR.cc
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+string v[10000];
+
+int f(int n, int x) {
+	switch (n) {
+	case 0:
+		return (2 * x) % 10000;
+	case 1:
+		return --x < 0 ? 9999 : x;
+	case 2:
+		return (x * 10 + x / 1000) % 10000;
+	case 3:
+		return (x + (x % 10) * 10000) / 10;
+	}
+}
+
+int main() {
+	int T;
+	cin >> T;
+	while (T--) {
+		int a, b;
+		bool isVisited[10000] = { false, };
+		cin >> a >> b;
+
+		queue<int> q;
+		q.push(a);
+		v[a] = "";
+		isVisited[a] = true;
+		while (!isVisited[b]) {
+			int k = q.front();
+			q.pop();
+			for(int i =0;i<4;i++){
+			int tmp = f(0, k);
+			if (!isVisited[tmp]) {
+				isVisited[tmp] = true;
+				q.push(tmp);
+				v[tmp] = v[k] + 'D';
+			}
+			tmp = f(1, k);
+			if (!isVisited[tmp]) {
+				isVisited[tmp] = true;
+				q.push(tmp);
+				v[tmp] = v[k] + 'S';
+			}
+			tmp = f(2, k);
+			if (!isVisited[tmp]) {
+				isVisited[tmp] = true;
+				q.push(tmp);
+				v[tmp] = v[k] + 'L';
+			}
+			tmp = f(3, k);
+			if (!isVisited[tmp]) {
+				isVisited[tmp] = true;
+				q.push(tmp);
+				v[tmp] = v[k] + 'R';
+			}
+			}
+		}
+		cout << v[b] << "\n";
+	}
+}

6fd0e172f1bbfb3d388b52915a759ffee2bd6aa5
Timeisfast <120894109+Timeisfast@users.noreply.github.com>
2024-02-24 01:39:30 +0900

    [Gold III] Title: 파티, Time: 8 ms, Memory: 9960 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cc
+++ 백준/Gold/1238. 파티/파티.cc
@@ -6,6 +6,7 @@ using namespace std;
 
 int N, M, X, sum[1001]{ 0, };
 vector<vector<int>> node;
+vector<vector<int>> r_node;
 
 vector<int> Dijkstra(int str) {
 	vector<int> dis(node.size(), INF);
@@ -33,37 +34,59 @@ vector<int> Dijkstra(int str) {
 	return dis;
 }
 
+vector<int> r_Dijkstra(int str) {
+	vector<int> dis(r_node.size(), INF);
+	dis[str] = 0;
+
+	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+	pq.push(make_pair(dis[str], str));
+
+	while (!pq.empty()) {
+		int n = pq.top().second;
+		pq.pop();
+
+		for (int i = 0; i < N + 1; i++) {
+			if (r_node[n][i] > 0) {
+				int k = dis[n] + r_node[n][i];
+				if (k < dis[i]) {
+					dis[i] = k;
+					pq.push(make_pair(dis[i], i));
+				}
+			}
+		}
+	}
+
+	return dis;
+}
+
 int main() {
-    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 	cin >> N >> M >> X;
 	node.resize(N + 1);
+	r_node.resize(N + 1);
 	for (int i = 0; i <= N; i++) {
 		node[i].resize(N + 1);
+		r_node[i].resize(N + 1);
 		for (int j = 0; j <= N; j++) {
 			node[i][j] = -1;
+			r_node[i][j] = -1;
 		}
 		node[i][i] = 0;
+		r_node[i][i] = 0;
 	}
 	int a, b, c;
 	while (M--) {
 		cin >> a >> b >> c;
 		node[a][b] = c;
+		r_node[b][a] = c;
 	}
 
-	for (int i = 1; i <= N; i++) {
-		if (i == X) {
-			vector<int> v = Dijkstra(i);
-			for (int j = 1; j <= N; j++) {
-				sum[j] += v[j];
-			}
-		}
-		else {
-			sum[i] += Dijkstra(i)[X];
-		}
-	}
-
+	vector<int> v1 = Dijkstra(X);
+	vector<int> v2 = r_Dijkstra(X);
 	int ans = 0;
 	for (int i = 1; i <= N; i++) {
+		sum[i] = v1[i] + v2[i];
 		if (sum[i] > ans) ans = sum[i];
 	}
 

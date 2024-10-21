bd122b1c12bc1ec710c075ba326191e3f605f6ef
Seongyun <120894109+Timeisfast@users.noreply.github.com>
2024-10-21 17:44:01 +0900

    [Silver III] Title: 프린터 큐, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.cc
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.cc
@@ -4,28 +4,31 @@ using namespace std;
 int doc[101];
 
 int main() {
-	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-	int T, N, M;
+	int T;
 	cin >> T;
+
 	while (T--) {
 		int imp[10] = { -1, 0, };
+		int N, M, k{ 9 }, ans{ 0 };
 		cin >> N >> M;
-		int a{ 9 }, ans{ 0 };
+
 		for (int i = 0; i < N; i++) {
 			cin >> doc[i];
 			imp[doc[i]]++;
 		}
+
 		for (int i = 0;; i++) {
 			if (i == N) i = 0;
-			while (imp[a] == 0) a--;
-			if (a == 0) break;
-			if (doc[i] == a) {
+			while (imp[k] == 0) k--;
+			if (k == 0) break;
+			if (doc[i] == k) {
 				ans++;
 				if (i == M) break;
 				doc[i] = 0;
-				imp[a]--;
+				imp[k]--;
 			}
 		}
+
 		cout << ans << "\n";
 	}
 }

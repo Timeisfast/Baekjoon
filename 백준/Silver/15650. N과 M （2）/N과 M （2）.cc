4578a18e323a35796985c9ae26fe0667cbb93c9f
Seongyun <120894109+Timeisfast@users.noreply.github.com>
2024-10-31 17:31:46 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N, M;
+vector<int> v;
+
+void print_vector() {
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << " ";
+	}
+	cout << "\n";
+}
+
+void fun(int x) {
+	if (v.size() == M) {
+		print_vector();
+		v.pop_back();
+		return;
+	}
+
+	for (int i = x + 1; i < N + 1; i++) {
+		v.push_back(i);
+		fun(i);
+	}
+
+	if (!v.empty()) {
+		v.pop_back();
+	}
+	return;
+}
+
+int main() {
+	cin >> N >> M;
+	fun(0);
+}

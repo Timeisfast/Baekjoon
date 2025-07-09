1bc066be069bb5b753aee7d9fa22aa3f6be4c402
Seongyun <120894109+Timeisfast@users.noreply.github.com>
2025-07-09 21:31:40 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 2.92 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -2,11 +2,10 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
 def solution(today, terms, privacies):
-    d = {types: validity for types, validity in (x.split() for x in terms)}
+    d = {x[0]: x[2:] for x in terms}
     final_date = [
-        datetime.strptime(date, "%Y.%m.%d")
-        + relativedelta(months=int(d[types]))
-        for date, types in (x.split() for x in privacies)
+        datetime.strptime(x[:-2], "%Y.%m.%d")
+        + relativedelta(months=int(d[x[-1]])) for x in privacies
     ]
     today = datetime.strptime(today, "%Y.%m.%d")
     answer = [(i + 1) for i, day in enumerate(final_date) if (day <= today)]
